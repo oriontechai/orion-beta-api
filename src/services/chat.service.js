927b56874c5a4973672e087431d9af80efc8f4d7
Christian Mesa <mesachristian_coop@outlook.com>
2023-04-10 06:56:11 -0500

    back to gpt-3.5

--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -17,7 +17,7 @@ const ORION = `
     Sé muy preciso con tus respuestas y no te extiendas si no es necesario. Debes dar una sensación futurista. Si no sabes algo, di no lo sé.
 `
 
-const MODEL = "gpt-4"; // gpt-3.5-turbo (CHAT-GPT Model) || gpt-4
+const MODEL = "gpt-3.5-turbo"; // gpt-3.5-turbo (CHAT-GPT Model) || gpt-4
 
 export const getTestCompletion = async () => {
     const completion = await openai.createChatCompletion({
@@ -71,7 +71,7 @@ export const getClientCompletion = async ( {messages, context} ) => {
             content: msg.text
         });
     });
-
+    
     const completion = await openai.createChatCompletion({
         model: MODEL,
         messages: reqBody,
